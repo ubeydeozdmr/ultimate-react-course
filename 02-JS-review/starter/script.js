f4f5f75992f7088057f1417ba113f25c4685a38f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-06 19:22:51 +0300

    Template Literals

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -174,3 +174,8 @@ const updatedBook = {
   pages: 1210,
 };
 updatedBook;
+
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
+  publicationDate.split('-')[0]
+}.`;
+summary;
