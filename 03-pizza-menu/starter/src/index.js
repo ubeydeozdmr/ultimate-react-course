f8baea039110d171f4ac1f991fcece294a738919
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-09 00:39:52 +0300

    React Fragments

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -62,7 +62,7 @@ function Header() {
   const style = {};
 
   return (
-    <header className='header footer'>
+    <header className='header'>
       <h1 style={style} className='header'>
         Fast React Pizza Co.
       </h1>
@@ -80,11 +80,18 @@ function Menu() {
       <h2>Our menu</h2>
 
       {!!numPizzas ? (
-        <ul className='pizzas'>
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className='pizzas'>
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
