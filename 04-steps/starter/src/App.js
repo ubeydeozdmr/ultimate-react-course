8d6a3d74808ac75d138dc08ae3630a774df82b5e
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-11 01:54:45 +0300

    More Reusability With the "Children" Prop

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -10,6 +10,14 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in content</p>
+        <p>✌️</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Read children prop</p>
+        <p>😎</p>
+      </StepMessage>
       {/* <Steps /> */}
     </div>
   );
@@ -49,10 +57,18 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <p className='message'>
-            Step {step}: {messages[step - 1]}
-            {/* {test.name} */}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className='buttons'>
+              <Button
+                textColor='#333'
+                bgColor='#e7e7e7'
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn how
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className='buttons'>
             <Button textColor='#fff' bgColor='#7950f2' onClick={handlePrevious}>
@@ -66,6 +82,15 @@ function Steps() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className='message'>
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
