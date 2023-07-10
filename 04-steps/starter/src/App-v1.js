4ed75a064fc2379ffc6a102531bdac6f7126a669
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-11 01:38:58 +0300

    The "children" Prop: Making a Reusable Button

--- 04-steps/starter/src/App-v1.js
+++ 04-steps/starter/src/App-v1.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
+      <Steps />
     </div>
   );
 }
@@ -55,27 +55,21 @@ function Steps() {
           </p>
 
           <div className='buttons'>
-            <Button textColor='#fff' bgColor='#7950f2' onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
-
-            <Button textColor='#fff' bgColor='#7950f2' onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
