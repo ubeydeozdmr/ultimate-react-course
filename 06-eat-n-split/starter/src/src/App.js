ee5fa80206df74befe3ec0e1fe2d268997b8c0be
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-11 19:17:38 +0300

    Building the Static App: List of Friends

--- 06-eat-n-split/starter/src/src/App.js
+++ 06-eat-n-split/starter/src/src/App.js
@@ -20,5 +20,48 @@ const initialFriends = [
 ];
 
 export default function App() {
-  return <div>Hello </div>;
+  return (
+    <div className='app'>
+      <div className='sidebar'>
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} width='48' />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>
+          You owe {friend.name} {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className='green'>
+          {friend.name} owes you {Math.abs(friend.balance)}€
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className=''>You and {friend.name} are even</p>
+      )}
+
+      <button className='button'>Select</button>
+    </li>
+  );
 }
