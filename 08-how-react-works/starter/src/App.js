dac9d5ef482cf8c10bace435cf338db088d35a5a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-20 23:03:49 +0300

    Instances and Elements in Practice

--- 08-how-react-works/starter/src/App.js
+++ 08-how-react-works/starter/src/App.js
@@ -26,6 +26,9 @@ export default function App() {
   );
 }
 
+console.log(<DifferentContent test={23} />);
+console.log(DifferentContent({ test: 23 }));
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
