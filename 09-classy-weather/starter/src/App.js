7603751ba557d83afb81531b02c8d192ac72b6ae
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-07-30 16:39:36 +0300

    Displaying the Weather

--- 09-classy-weather/starter/src/App.js
+++ 09-classy-weather/starter/src/App.js
@@ -92,9 +92,61 @@ class App extends React.Component {
         <button onClick={this.fetchWeather}>Get weather</button>
 
         {this.state.isLoading && <p className='loader'>Loading...</p>}
+
+        {this.state.weather.weathercode && (
+          <Weather
+            weather={this.state.weather}
+            location={this.state.location}
+          />
+        )}
       </div>
     );
   }
 }
 
 export default App;
+
+class Weather extends React.Component {
+  render() {
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
+
+    return (
+      <div>
+        <h2>Weather {this.props.location}</h2>
+        <ul className='weather'>
+          {dates.map((date, i) => (
+            <Day
+              date={date}
+              min={min[i]}
+              max={max[i]}
+              code={codes[i]}
+              key={date}
+              isToday={i === 0}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Day extends React.Component {
+  render() {
+    const { date, min, max, code, isToday } = this.props;
+
+    return (
+      <li className='day'>
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? 'Today' : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+        </p>
+      </li>
+    );
+  }
+}
