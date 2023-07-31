daf0f0234ab835dc59c67e38dd4d35381462967f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-01 00:43:15 +0300

    Starting a New Quiz

--- 10-react-quiz/starter/src/App.js
+++ 10-react-quiz/starter/src/App.js
@@ -4,6 +4,7 @@ import Main from './Main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
@@ -25,6 +26,9 @@ function reducer(state, action) {
         ...state,
         status: 'error',
       };
+    case 'start':
+      return { ...state, status: 'active' };
+
     default:
       throw new Error('Action unknown');
   }
@@ -41,6 +45,7 @@ export default function App() {
       .then((data) => dispatch({ type: 'dataReceived', payload: data }))
       .catch((err) => dispatch({ type: 'dataFailed' }));
   }, []);
+
   return (
     <div className='app'>
       <Header />
@@ -48,7 +53,10 @@ export default function App() {
       <Main className='main'>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question />}
       </Main>
     </div>
   );
