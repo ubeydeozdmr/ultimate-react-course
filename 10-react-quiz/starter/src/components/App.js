317cdd74e97b5133969ab2db2c543fce0e40f081
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-01 13:00:48 +0300

    Finishing a Quiz

--- 10-react-quiz/starter/src/components/App.js
+++ 10-react-quiz/starter/src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
   questions: [],
@@ -16,6 +17,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -45,10 +47,13 @@ function reducer(state, action) {
             : state.points,
       };
     case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
+    case 'finish':
       return {
         ...state,
-        index: state.index + 1,
-        answer: null,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
       };
 
     default:
@@ -57,10 +62,8 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState,
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce(
@@ -99,8 +102,20 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
           </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
