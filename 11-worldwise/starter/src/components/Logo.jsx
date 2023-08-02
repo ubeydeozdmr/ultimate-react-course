cab39f9cdababe498eed3aaacda51dd5549749f2
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-03 01:18:28 +0300

    Building the Pages

--- 11-worldwise/starter/src/components/Logo.jsx
+++ 11-worldwise/starter/src/components/Logo.jsx
@@ -1,7 +1,12 @@
-import styles from "./Logo.module.css";
+import { Link } from 'react-router-dom';
+import styles from './Logo.module.css';
 
 function Logo() {
-  return <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />;
+  return (
+    <Link to='/' className={styles.logo}>
+      <img src='/logo.png' alt='WorldWise logo' className={styles.logo} />
+    </Link>
+  );
 }
 
 export default Logo;
