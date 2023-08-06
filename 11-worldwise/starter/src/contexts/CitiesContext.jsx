1d0f8ee3653ea19e097ea9d466e30b8f5cbe8a8d
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-06 15:09:10 +0300

    Consuming the CitiesContext

--- 11-worldwise/starter/src/contexts/CitiesContext.jsx
+++ 11-worldwise/starter/src/contexts/CitiesContext.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -37,8 +37,16 @@ function CitiesProvider({ children }) {
   );
 }
 
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error('CitiesContext was used outside the CitiesProvider');
+  return context;
+}
+
 CitiesProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-export { CitiesProvider };
+// eslint-disable-next-line react-refresh/only-export-components
+export { CitiesProvider, useCities };
