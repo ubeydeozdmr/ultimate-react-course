0bc39cc1b67147c617c939fa6bc8fa3e826f630e
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-06 14:54:54 +0300

    Back to "WorldWise": Creating a CitiesContext

---
+++ 11-worldwise/starter/src/contexts/CitiesContext.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import { createContext, useState, useEffect } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+CitiesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export { CitiesProvider };
