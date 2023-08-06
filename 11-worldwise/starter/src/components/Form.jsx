78e1db499f2bca0e4f9da5c78a701c5e49b5c4ae
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-06 16:21:15 +0300

    Finishing the City View

--- 11-worldwise/starter/src/components/Form.jsx
+++ 11-worldwise/starter/src/components/Form.jsx
@@ -1,10 +1,10 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from 'react';
+import Button from './Button';
+import BackButton from './BackButton';
 
 import styles from './Form.module.css';
-import Button from './Button';
-import { useNavigate } from 'react-router-dom';
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -15,8 +15,6 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate();
-
   const [cityName, setCityName] = useState('');
   const [country, setCountry] = useState('');
   const [date, setDate] = useState(new Date());
@@ -54,15 +52,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type='primary'>Add</Button>
-        <Button
-          type='back'
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          <span>&larr;</span> Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
