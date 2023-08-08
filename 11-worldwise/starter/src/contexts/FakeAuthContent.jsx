180f0ad9713e0f0f37826c479e648b26b01eed71
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-09 01:28:18 +0300

    Adding Fake Authenication: Implementing "Login"

--- 11-worldwise/starter/src/contexts/FakeAuthContent.jsx
+++ 11-worldwise/starter/src/contexts/FakeAuthContent.jsx
@@ -61,10 +61,12 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error('AuthContext was used outside AuthProvider');
+  return context;
 }
 
 AuthProvider.propTypes = {
   children: propTypes.node.isRequired,
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export { AuthProvider, useAuth };
