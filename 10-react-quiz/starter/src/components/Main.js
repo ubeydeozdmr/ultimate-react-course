b987197f3e0315315e6cd612f511ce59ac4e5216
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-09 14:35:58 +0300

    Used ContextAPI for Challenge

--- 10-react-quiz/starter/src/components/Main.js
+++ 10-react-quiz/starter/src/components/Main.js
@@ -1,5 +1,35 @@
-function Main({ children }) {
-  return <main className='main'>{children}</main>;
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+import NextButton from './NextButton';
+import Progress from './Progress';
+import FinishScreen from './FinishScreen';
+import Footer from './Footer';
+import Timer from './Timer';
+import { useQuiz } from '../contexts/QuizContext';
+
+function Main() {
+  const { status } = useQuiz();
+
+  return (
+    <main className='main'>
+      {status === 'loading' && <Loader />}
+      {status === 'error' && <Error />}
+      {status === 'ready' && <StartScreen />}
+      {status === 'active' && (
+        <>
+          <Progress />
+          <Question />
+          <Footer>
+            <Timer />
+            <NextButton />
+          </Footer>
+        </>
+      )}
+      {status === 'finished' && <FinishScreen />}
+    </main>
+  );
 }
 
 export default Main;
