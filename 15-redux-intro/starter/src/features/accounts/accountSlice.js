b852ed8c871f435f0bbdc625d287fa535ba32b9e
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-08-12 16:08:43 +0300

    Creating the Account Slice

--- 15-redux-intro/starter/src/features/accounts/accountSlice.js
+++ 15-redux-intro/starter/src/features/accounts/accountSlice.js
@@ -1,11 +1,55 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = '';
+    },
+  },
+});
+
+console.log(accountSlice);
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+console.log(requestLoan(1000, 'Buy a car'));
+
+export default accountSlice.reducer;
+
+/*
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -70,3 +114,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: 'account/payLoan' };
 }
+*/
