3cbfc46ff52a02d7c96eb65ed7f3b28acd4d496c
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-10-02 01:06:25 +0300

    Fetching Orders

--- 16-fast-react-pizza/starter/src/ui/Header.jsx
+++ 16-fast-react-pizza/starter/src/ui/Header.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <div>
       <Link to='/'>Fast React Pizza Co.</Link>
+
+      <SearchOrder />
 
       <p>Jonas</p>
     </div>
