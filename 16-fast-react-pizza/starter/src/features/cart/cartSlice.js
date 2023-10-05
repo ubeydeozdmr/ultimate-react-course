1c7a5dd7fafbce30a7ed7d6cef1428c6c2ffb91f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-10-06 01:44:05 +0300

    Deleting Cart Items

--- 16-fast-react-pizza/starter/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/starter/src/features/cart/cartSlice.js
@@ -67,3 +67,8 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) => {
+  const item = state.cart.cart.find((item) => item.pizzaId === id);
+  return item ? item.quantity : 0;
+};
