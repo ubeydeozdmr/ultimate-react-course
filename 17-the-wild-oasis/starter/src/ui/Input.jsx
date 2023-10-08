fe9b6a57a1acee25b66212a5763c11de7670b713
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-10-08 12:39:48 +0300

    Building the App Layout

--- 17-the-wild-oasis/starter/src/ui/Input.jsx
+++ 17-the-wild-oasis/starter/src/ui/Input.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   border: 1px solid #ddd;
-  background-color: var(--color-gray-0);
+  background-color: var(--color-grey-0);
   border-radius: var(--border-radius-sm);
   padding: 0.8rem 1.2rem;
   box-shadow: var(--shadow-sm);
