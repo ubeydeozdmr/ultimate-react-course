9bc1501d48e45d46e2fae20ce0d4aa7aae384d6f
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-10-26 15:30:08 +0300

    Client-Side Filtering: Filtering Cabins

--- 17-the-wild-oasis/starter/src/ui/Filter.jsx
+++ 17-the-wild-oasis/starter/src/ui/Filter.jsx
@@ -1,4 +1,6 @@
-import styled, { css } from "styled-components";
+import PropTypes from 'prop-types';
+import { useSearchParams } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 const StyledFilter = styled.div`
   border: 1px solid var(--color-grey-100);
@@ -33,3 +35,39 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchParams.set('discount', value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+Filter.propTypes = {
+  filterField: PropTypes.string,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default Filter;
