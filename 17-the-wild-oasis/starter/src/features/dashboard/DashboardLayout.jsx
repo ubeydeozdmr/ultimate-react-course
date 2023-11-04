30d68570f08c35467895877e26bdd8c3cd9a8076
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-11-04 21:47:49 +0300

    Displaying a Pie Chart

--- 17-the-wild-oasis/starter/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/starter/src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays';
 import { useCabins } from '../cabins/useCabins';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -34,7 +35,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today&apos;s activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
