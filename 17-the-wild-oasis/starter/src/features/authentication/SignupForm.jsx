a7cd14b6c21037906e774cd26c3507bd5388c480
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-11-04 14:40:10 +0300

    Building the Sign Up Form

--- 17-the-wild-oasis/starter/src/features/authentication/SignupForm.jsx
+++ 17-the-wild-oasis/starter/src/features/authentication/SignupForm.jsx
@@ -1,32 +1,86 @@
-import Button from "../../ui/Button";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import { useForm } from 'react-hook-form';
+import Button from '../../ui/Button';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { register, formState, getValues, handleSubmit } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form noValidate onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label='Full name' error={errors?.fullName?.message}>
+        <Input
+          type='text'
+          id='fullName'
+          {...register('fullName', {
+            required: 'Full name is required',
+            minLength: {
+              value: 3,
+              message: 'Full name must be at least 3 characters long',
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label='Email address' error={errors?.email?.message}>
+        <Input
+          type='email'
+          id='email'
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Please provide a valid email address',
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label='Password (min 8 characters)'
+        error={errors?.password?.message}
+      >
+        <Input
+          type='password'
+          id='password'
+          {...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters long',
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label='Repeat password' error={errors?.passwordConfirm?.message}>
+        <Input
+          type='password'
+          id='passwordConfirm'
+          {...register('passwordConfirm', {
+            required: 'Password confirmation is required',
+            validate: (value) =>
+              value === getValues().password || 'Passwords need to match',
+            minLength: {
+              value: 8,
+              message:
+                'Password confirmation must be at least 8 characters long',
+            },
+          })}
+        />
       </FormRow>
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button variation='secondary' type='reset'>
           Cancel
         </Button>
         <Button>Create new user</Button>
