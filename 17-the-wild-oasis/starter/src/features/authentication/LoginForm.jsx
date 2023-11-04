723ef5f72f7a46b4921a3549613f3095c858140a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2023-11-04 23:26:44 +0300

    Deploying to Netlify

--- 17-the-wild-oasis/starter/src/features/authentication/LoginForm.jsx
+++ 17-the-wild-oasis/starter/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('ubeyde@example.com');
-  const [password, setPassword] = useState('asdf1234');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
